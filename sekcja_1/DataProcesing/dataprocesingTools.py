f262b3fa76e8d2eee1f8466b648f5c782e0bd840
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-06-09 10:58:23 +0200

    take care of missing data

--- sekcja_1/DataProcesing/dataprocesingTools.py
+++ sekcja_1/DataProcesing/dataprocesingTools.py
@@ -1,10 +1,22 @@
+#import libraries
 import numpy as np 
 import matplotlib.pyplot as plt 
 import pandas as pd
 
+#read data 
 dataset = pd.read_csv("Data.csv")
 x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
+#print to screen
 print(x)
 print(y)
+
+## take  care of missing data 
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(x[ : ,1:3])
+x[:,1:3] = imputer.transform(x[ : ,1:3])
+
+print(x)
