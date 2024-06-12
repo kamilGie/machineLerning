c435042a9bcc27684ccbfe9e820555db64d1c84f
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-06-12 21:54:31 +0200

    xd

--- sekcja_1/DataProcesing/dataprocesingTools.py
+++ sekcja_1/DataProcesing/dataprocesingTools.py
@@ -28,16 +28,9 @@ y = le.fit_transform(y)
 #split to traing and test models
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state = 1)
-print("testing sets start ",end="\n\n\n")
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
 
 #future scaling 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train[:,3:] =sc.fit_transform(X_train[:,3:])
 X_test[:,3:] =sc.transform(X_test[:,3:])
-print(X_train)
-print(X_test)
