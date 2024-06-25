4c397f4f58f6dacfc99fe41c4e2eff79d6ef8036
Kamil Gieras <104592814+kamilGie@users.noreply.github.com>
2024-06-25 12:31:03 +0200

    logisitc middle

---
+++ sekcja_3/logisticRegresion/logistiRegression.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0).fit(X_train,Y_train)
+
+#30 y and 87 000 dolars
+
+#print(classifier.predict(sc.transform([[30, 87000]])))
+y_pred = classifier.predict(X_test)
+
+
